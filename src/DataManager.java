62a50dd0cf311559adf5b8b1a63ba895b1abf79f
dunc415 <duncanchet2001@hotmail.com>
2021-05-01 15:51:53 -0300

    Messy

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,7 +1,13 @@
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class DataManager {
 	
@@ -14,7 +20,7 @@ public class DataManager {
 	
 	
 	// Might want to change this to have be a method.
-	public void connectDB(String NAME_OF_DB){
+	public boolean connectDB(String NAME_OF_DB){
 		
 		try{
 			// String path = "INSERT_THE_PATH_FOR_STORING_DATABASES";
@@ -30,13 +36,14 @@ public class DataManager {
 				if(connection != null){
 					connection.close();
 				}
-			}catch(SQLExcpetion ex){
+			}catch(SQLException ex){
 				System.out.println(ex.getMessage());
 			}
 		}
+		return true;
 	}
 	
-	public void createDB(String NAME_OF_DB){
+	public boolean createDB(String NAME_OF_COLLECTOR_DB){
 		
 		try{
 			String collectorPath = "jdbc:sqlite:C:/sqlite/db/" + NAME_OF_COLLECTOR_DB + ".db";
@@ -46,17 +53,19 @@ public class DataManager {
 			Statement state = connection.createStatement();
 			
 			// SQL Query for adding Collection Name into the database.
-			String sqlQuery = "insert into CollectionInfo values( , '" + NAME_OF_DB + "');";
+			String sqlQuery = "insert into CollectionInfo values( , '" + NAME_OF_COLLECTOR_DB + "');";
 			
 			state.executeUpdate(sqlQuery);
-			
+
 		}catch(SQLException ex){
-			System.out.println(e.getMeassage());
+			System.out.println(ex.getMessage());
+			return false;
 		}
 		
-		String path = "jdbc:sqlite:C:/sqlite/db/" + NAME_OF_DB + ".db";
+		String path = "jdbc:sqlite:C:/sqlite/db/" + NAME_OF_COLLECTOR_DB + ".db";
 		
-		try(connection = DriverManager.getConnection(path)){
+		try{
+			connection = DriverManager.getConnection(path);
 			if(connection != null){
 				DatabaseMetaData meta = connection.getMetaData();
 				System.out.println("The driver name is " + meta.getDriverName());
@@ -64,8 +73,10 @@ public class DataManager {
 				numOfDB++;
 			}
 		}catch(SQLException e){
-			System.out.println(e.getMeassage());
+			System.out.println(e.getMessage());
+			return false;
 		}
+		return true;
 	}
 
 	public int getNumberOfCollections(){
@@ -74,12 +85,12 @@ public class DataManager {
 	
 	public ObservableList<Album> getAlbums(){
 		
-		private ObservableList<Album> list = FXCollections.observableArrayList();
+		ObservableList<Album> list = FXCollections.observableArrayList();
 		
 		try{
 		
 			Statement state = connection.createStatement();
-			String queryForAlbums = "select * from Albums;"
+			String queryForAlbums = "select * from Albums;";
 			
 			ResultSet resultSet = state.executeQuery(queryForAlbums);
 			
@@ -98,5 +109,6 @@ public class DataManager {
 		}catch(SQLException e){
 			System.err.println("SQL Error: getAlbums()");
 		}
+		return list;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.controls.*;
 import javafx.scene.text.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.geometry.*;
 import javafx.event.ActionEvent;
 
 
-public class Main {
+public class Main extends Application{
 	
 	DataManager dm = new DataManager();
 	
@@ -18,7 +18,7 @@ public class Main {
 		Label lblTitle, lblNewCollection, lblExistingCollection;
 		Button btnContCollection, btnNewCollection;
 		TextField tfNameOfCollection;
-		ChoiceBox cbCollections;
+		ChoiceBox<String> cbCollections;
 		BorderPane borderpane;
 		
 		// Will need to add different font and style using CSS
@@ -40,9 +40,8 @@ public class Main {
 		// Will need to create a public variable that is a number that keeps track
 		// of how many databases/collections have been created. We will use this
 		// variable to gather the names of the collections from the database that stores the collection names.
-		
-		cbCollections = new ChoiceBox("Pick your Collection...");
-		cbCollections.setAlignment(Pos.CENTER);
+		 
+		cbCollections = new ChoiceBox();
 		
 		// BUTTONS - DEFINING
 		
@@ -52,6 +51,8 @@ public class Main {
 			if(dm.connectDB(cbCollections.getValue())){
 				//Set Stage to Add Artist/Album screen
 			}else{
+			
+			}
 		});
 		btnNewCollection = new Button("New Collection");
 		btnNewCollection.setAlignment(Pos.CENTER);
@@ -59,34 +60,46 @@ public class Main {
 			if(dm.createDB(tfNameOfCollection.getText())){
 				//Set Stage to Add Artist/Album screen
 			}else{
+
+			}
 		});
 		
 		// VBOXs and HBOXs
 		
 		VBox vboxCollectionExist = new VBox(lblExistingCollection, cbCollections, btnContCollection);
 		vboxCollectionExist.setSpacing(20);
+		vboxCollectionExist.setAlignment(Pos.CENTER);
 		
 		VBox vboxNewCollection = new VBox(lblNewCollection, tfNameOfCollection, btnNewCollection);
 		vboxNewCollection.setSpacing(20);
+		vboxNewCollection.setAlignment(Pos.CENTER);
 		
 		// BORDERPANE
 		
 		borderpane = new BorderPane();
+		
 		borderpane.setTop(lblTitle);
+		BorderPane.setAlignment(lblTitle, Pos.CENTER);
+		BorderPane.setMargin(lblTitle, new Insets(15));
+
 		borderpane.setLeft(vboxCollectionExist);
+		BorderPane.setAlignment(vboxCollectionExist, Pos.CENTER);
+		BorderPane.setMargin(vboxCollectionExist, new Insets(15));
+
 		borderpane.setRight(vboxNewCollection);
+		BorderPane.setAlignment(vboxNewCollection, Pos.CENTER);
+		BorderPane.setMargin(vboxNewCollection, new Insets(15));
 		
 		
 		// SCENE/STAGE
 		
-		Scene scene = new Scene(btnContCollection, 500, 500);
-		mainStage = new setScene(scene);
+		Scene scene = new Scene(borderpane, 600, 400);
+		mainStage.setScene(scene);
 		mainStage.show();
 		
 	}
 	
 	public static void main(String[] args){
-		launch(args);
+		Application.launch(args);
 	}
-	
 }
